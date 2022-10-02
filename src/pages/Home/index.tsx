96c868cb6b39b203a24eaab3c36d6c47c581544b
btbrad <24836323+btbrad@users.noreply.github.com>
2022-10-02 21:38:00 +0800

    feat: 渲染模板页面

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>hello-ssr</h1>
+      <button onClick={() => alert('hello ssr')}>alert</button>
+    </div>
+  )
+}
+
+export default Home
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,20 +1,27 @@
-import express from "express";
-import childProcess from "child_process";
+import express from 'express'
+import childProcess from 'child_process'
+import { renderToString } from 'react-dom/server'
+import Home from '@/pages/Home'
+import path from 'path'
 
-const app = express();
+const app = express()
+const content = renderToString(<Home />)
 
-app.get("*", (req, res) => {
+app.use(express.static(path.resolve(process.cwd(), 'client_build')))
+
+app.get('*', (req, res) => {
   res.send(`
     <html
       <body>
-        <div>hello-ssr</div>
+        <div id="root">${content}</div>
+        <script src="/index.js"></script>
       </body>
     </html>
-  `);
-});
+  `)
+})
 
 app.listen(3000, () => {
-  console.log("ssr-server listen on 3000");
-});
+  console.log('ssr-server listen on 3000')
+})
 
-childProcess.exec("start http://127.0.0.1:3000");
+childProcess.exec('start http://127.0.0.1:3000')

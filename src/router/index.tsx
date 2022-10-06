fc1d7b843121c236d995d5239a8ff0ec432121ff
btbrad <24836323+btbrad@users.noreply.github.com>
2022-10-06 21:05:00 +0800

    feat: 实现ssr

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,11 +4,19 @@ import Demo from '@/pages/Demo'
 interface IRouter {
   path: string
   element: JSX.Element
+  loadData?: (store: any) => any
 }
 
 const router: Array<IRouter> = [
-  { path: '/', element: <Home /> },
-  { path: '/demo', element: <Demo /> },
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/demo',
+    element: <Demo />,
+    loadData: Demo.getInitProps,
+  },
 ]
 
 export default router
